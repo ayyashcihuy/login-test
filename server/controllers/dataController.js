33b6d052d4c9cce2b934ea7a15006a9229f4d3db
ayyashcihuy <ayyashcihuy@gmail.com>
2022-03-04 18:16:14 +0700

    adding async func for using redis + caching

--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const Redis = require("ioredis");
 const redis = new Redis();
+const DEFAULT_EXPIRATION = 60;
 
 class DataSaver {
   static getData(req, res, next) {
@@ -16,7 +17,7 @@ class DataSaver {
         })
           .then(({ data }) => {
             response = JSON.stringify(data);
-            return redis.set("data", response);
+            return redis.setex("data", 60, response);
           })
           .then(() => {
             console.log(
@@ -34,8 +35,24 @@ class DataSaver {
 
   static RedisRefresher(req, res, next) {
     res.status(200).json({ message: "Data di Redis telah dibersihkan" });
-    redis.lpush;
-    return redis.del("data");
+    return redis.flushall;
+  }
+
+  static getDataAsync(req, res, next) {
+    const albumId = req.query.albumId;
+    redis.get("photos", async (err, photos) => {
+      if (err) res.status(500).json("Samtingwong");
+      if (photos !== null) {
+        return res.status(400).json(JSON.parse(photos));
+      } else {
+        const { data } = await axios(
+          "https://jsonplaceholder.typicode.com/photos",
+          { params: { albumId } }
+        );
+        redis.setex("photos", DEFAULT_EXPIRATION, JSON.stringify(data));
+        return res.status(400).json(data);
+      }
+    });
   }
 }
 
--- server/routes/dataRouter.js
+++ server/routes/dataRouter.js
@@ -3,5 +3,6 @@ const dataRouter = require("express").Router();
 
 dataRouter.get("/pokemon", DataSaver.getData);
 dataRouter.get("/clean", DataSaver.RedisRefresher);
+dataRouter.get("/photos", DataSaver.getDataAsync);
 
 module.exports = dataRouter;
